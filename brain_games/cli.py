37320c2db4439995df30dedfe1dd1bdbb3d7d5b6
Artem <ar180595@mail.ru>
2021-05-23 11:09:24 +0300

    fix

---
+++ brain_games/cli.py
@@ -0,0 +1,11 @@
+import prompt
+def main():
+
+    if __name__ == '__main__':
+
+        main()
+
+def welcome_user():
+	print('Welcome to the Brain Games!')
+	name = prompt.string('May I have your name? ')
+	print ('Hello,', name)
